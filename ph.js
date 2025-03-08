6470d9774036f1d066dd7108ae2b7c112d7aec12
Lavesh Dongre <85252596+laveshdongre@users.noreply.github.com>
2025-03-08 20:42:28 +0530

    Delete ph.js

--- ph.js
+++
@@ -1,8 +0,0 @@
-$('.popup-youtube, .popup-text').magnificPopup({
-    disableOn: 320,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: true
-});
